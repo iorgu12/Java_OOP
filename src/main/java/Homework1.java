e79c90fc3024152a3e312acf7f495e1ba7f0e760
iorgu12 <iorgubarden@yahoo.com>
2020-11-09 23:30:17 +0100

    git commit -a

--- src/main/java/Homework1.java
+++ src/main/java/Homework1.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class Homework2 {
+public class Homework1 {
     
     private static int[] readFile() {
         try {
-            Scanner sc = new Scanner(new File("input.txt"));
+            Scanner sc = new Scanner(new File("./src/main/java/homework1-correct/input.txt"));
             int a = sc.nextInt();
-            int b = sc.nextInt()
-            return new int[] { a, b };
+            int b = sc.nextInt();
+            return new int[]{a, b};
         } catch (FileNotFoundException ex) {
             return null;
         }
@@ -18,8 +18,8 @@ public class Homework2 {
         try {
             Writer writer = new BufferedWriter(
                     new OutputStreamWriter(
-                        new FileOutputStream("output.txt"), "ascii"));
-            writer.write(new Integer(number).toString();)
+                        new FileOutputStream("./src/main/java/homework1-correct/output.txt"), "ascii"));
+            writer.write(new Integer(number).toString());
             writer.close();
           } catch (IOException ex) {
               
@@ -28,7 +28,7 @@ public class Homework2 {
 
     
     public static void main(String[] args) {
-        int[] vals = readFile()
+        int[] vals = readFile();
         long mult = vals[0] * vals[1];
-        writeFile(mult);
-    }
+        writeFile((int)mult);
+    }}
